57fa54bc41b62c545d4b2d83396ab46eb3c81316
mayankkoli <mayankkoli@mayankkoli-mac.local>
2023-02-23 10:41:39 +0530

    add k8

---
+++ k8-manifests/flask-app1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#Importing module of flask framework to run app on web
+from flask import Flask, render_template
+import datetime
+
+
+# Create the application.
+app = Flask(__name__)
+
+
+# Displays the index page accessible at '/'
+@app.route("/")
+def index():
+    print ("Hello World !!! - v2")
+    return "This is first deployment. Kindly use /image uri to get output"
+
+
+# This code is finish and ready to server on port 80
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
